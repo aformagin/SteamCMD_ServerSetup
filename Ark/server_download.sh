dda8999a8f6da641dc6949b7a4edec677147bc21
aformagin <Austin.Formagin117@gmail.com>
2022-08-19 10:49:56 -0400

    Updated server_download.sh
    Needs testing

--- Ark/server_download.sh
+++ Ark/server_download.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-ECHO "TODO"
+echo "Creating directory for Ark (CURRENT_DIR/Servers/Ark/)"
+mkdir -p Servers/Ark
+echo "Starting ARK:SE Server Download"
+WORKING_DIR = pwd
+INSTALL = '$WORKING_DIR/Servers/Ark'
+steamcmd +force_install_dir $INSTALL +login anonymous +app_update 376030 +quit
